f342421aae61e2b662be5f2fdabba4b98cf37e5e
donavon <github@donavon.com>
2016-06-16 11:51:06 -0400

    Support the "default" method of importing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "matchmedia-polyfill";
 import "matchmedia-polyfill/matchMedia.addListener";
-import ResponsiveComponent from "./ui/ResponsiveComponent";
+import ResponsiveComponentDefault from "./ui/ResponsiveComponent";
 
-export { ResponsiveComponent };
+class ResponsiveComponent extends ResponsiveComponentDefault {
+    constructor(props) {
+        super(props);
+        console.warn( //eslint-disable-line no-console
+            "The following format is deprecated:\n" +
+            "  import { ResponsiveComponent } from 'react-responsive-component';\n" +
+            "Please use this format instead:\n" +
+            "  import RenderIf from 'react-responsive-component';\n" +
+            "This will stop working completely in verion 2.x"
+        );
+    }
+}
+
+export default ResponsiveComponentDefault; // This is the new way.
+export { ResponsiveComponent }; // This is the old way.
